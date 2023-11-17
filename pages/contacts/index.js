1b7b5e9e332c6f40951e3012fb1831aefd43ff4e
Fiqre-Ab <Fiqreyesusabraham@gmail.com>
2023-11-16 21:14:56 -0600

    fixing bugs

--- pages/contacts/index.js
+++ pages/contacts/index.js
@@ -5,3 +5,25 @@ $(document).ready(function(){
     return false;
   });
 });
+
+
+function openmenu()
+              {
+                  document.getElementById("side-menu").style.display="block";
+                  document.getElementById("menu-btn").style.display="none";
+                  document.getElementById("close-btn").style.display="block";
+              }
+              function closemenu()
+              {
+                  document.getElementById("side-menu").style.display="none";
+                  document.getElementById("menu-btn").style.display="block";
+                  document.getElementById("close-btn").style.display="none";
+              }
+                   function toggle_visibility() {
+               var e = document.getElementById('feedback-main');
+               if(e.style.display == 'block')
+                  e.style.display = 'none';
+               else
+                  e.style.display = 'block';
+            }
+    
